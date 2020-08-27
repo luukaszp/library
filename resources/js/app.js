c2d668af60a1bfd174701d0dd244c3803b4785dd
luukaszp <luukasz.poterala@wp.pl>
2020-08-27 09:12:37 +0200

    logout when token expires

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,8 @@
+import { get } from 'lodash';
 import Vue from 'vue';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
+import VueSweetalert2 from 'vue-sweetalert2';
 import App from './App.vue';
 import vuetify from './plugins/vuetify';
 import router from './router';
@@ -24,8 +26,58 @@ axios.interceptors.request.use(
   (error) => Promise.reject(error)
 );
 
+/* axios.interceptors.response.use((response) => response,
+  (error) => {
+    if (error.response.status !== 401) {
+      return Promise.reject(error);
+    }
+    if (error.response.status === 401) {
+      axios({
+        method: 'POST',
+        url: 'http://127.0.0.1:8000/api/refresh',
+        data: {
+          access_token: localStorage.getItem('access_token')
+        }
+      })
+        .then((response) => {
+          if (response.status === 201) {
+            localStorage.removeItem('access_token');
+            // 1) put token to LocalStorage
+            const token = localStorage.setItem('access_token', response.data.access_token);
+
+            // 2) Change Authorization header
+            axios.defaults.headers.Authorization = `Bearer ${token}`;
+            axios.defaults.headers['Access-Control-Allow-Origin'] = '*';
+            // window.location.reload();
+          }
+        });
+    }
+  }); tutaj miał być warunek, czy chce się ponownie zalogować czy też nie, ale to .js */
+
+axios.interceptors.response.use((response) => response,
+  (error) => {
+    if (error.response.status !== 401) {
+      return Promise.reject(error);
+    }
+    if (error.response.status === 401) {
+      Vue.swal({
+        title: 'Sesja wygasła',
+        text: 'Za chwilę zostaniesz wylogowany',
+        icon: 'warning',
+        confirmButtonText: 'Rozumiem'
+      }).then((result) => {
+        if (result.value) {
+          Vue.swal('Wylogowano', 'Pomyślnie wylogowano!', 'success');
+          localStorage.removeItem('access_token');
+          router.push('/login');
+        }
+      });
+    }
+  });
+
 Vue.config.productionTip = false;
 
+Vue.use(VueSweetalert2);
 Vue.use(VueAxios, axios);
 
 new Vue({
--- routes/api.php
+++ routes/api.php
@@ -16,12 +16,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('loginReader', 'AuthController@loginReader');
 Route::post('loginWorker', 'AuthController@loginWorker');
+Route::post('refresh', 'AuthController@refresh');
 
 Route::group(
     ['middleware' => 'auth.jwt'], function () {
         Route::get('logout', 'AuthController@logout');
         Route::post('logout', 'AuthController@logout');
-        Route::post('refresh', 'AuthController@refresh');
+        //Route::post('refresh', 'AuthController@refresh');
         Route::post('me', 'AuthController@me');
     }
 );
@@ -36,10 +37,6 @@ Route::group(
         Route::put('roles/edit/{id}', 'UserController@editRoles');
     }
 );
-
-/*Route::group(['middleware' => ['auth.jwt', 'worker']], function () {
-
-});*/
 
 Route::group(
     ['middleware' => ['auth.jwt', 'worker']], function () {
