71707e7fdc599baac7b534973bb5024421afcec6
luukaszp <luukaszpoterala@wp.pl>
2021-10-19 17:13:41 +0200

    pls

--- resources/js/components/Profile/Profile.vue
+++ resources/js/components/Profile/Profile.vue
@@ -108,7 +108,12 @@ export default {
           }
         };
 
-        axios.post('/api/user/profile/upload', form, { headers: form.getHeaders() })
+        let url = 'https://library-site.herokuapp.com/api/user/profile/upload';
+        let request = new XMLHttpRequest();
+        request.open('POST', url);
+        request.send(form);
+
+        /*axios.post('/api/user/profile/upload', form, { headers: form.getHeaders() })
           .then((response) => {
             if (response.data.success == true) {
               this.$swal('Zmieniono', 'Pomyślnie zmieniono awatar!', 'success');
@@ -118,7 +123,7 @@ export default {
           })
           .catch((error) => {
             console.log(error);
-          });
+          });*/
           this.$store.dispatch('fetchOneReader', this.user_id);
       }
     },
